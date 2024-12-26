0c15d3b240d5ad0fd5326ce0ece5a1cd64233682
Natihermida <hermidaltamarnati@gmail.com>
2024-12-26 18:35:13 +0100

    cambios_css_js

---
+++ registro.js
@@ -0,0 +1,9 @@
+var inputPass=document.getElementById("pass");
+var inputRepass=document.getElementById("repass");
+/*crear boton*/
+var bntCrear=document.getElementById("bntCrear");
+inputPass.onchange=function(){
+    var txtPass=inputPass.value;
+    console.log(txtPass);
+
+}
