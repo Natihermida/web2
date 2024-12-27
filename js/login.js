d1814bebdbca36c187a862634f75ccb6cfca4c90
Natihermida <hermidaltamarnati@gmail.com>
2024-12-27 19:03:59 +0100

    main_ultima_actualizacion

--- js/login.js
+++ js/login.js
@@ -1,2 +1,10 @@
 /*capturar evento sublimt del formulario*/
 var formulario=document.getElementById("formulario")
+formulario.onsublimit=function(e){
+    e.preventDefault();
+    var usuario=document.getElementById("usuario").value;
+    var password=document.getElementById("password").value;
+    if(usuario=="admin"&& password=="admin"){
+        window.location.href="main.html";
+    }
+}
